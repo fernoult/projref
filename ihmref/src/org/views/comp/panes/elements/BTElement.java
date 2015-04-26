9e63212144747c3181f5ba00a37819fe690c8c4a
fernoult <francois.buza@gmail.com>
2015-04-26 13:09:42 +0200

    Avanecemnt du jour, encore ,et encore...

---
+++ ihmref/src/org/views/comp/panes/elements/BTElement.java
@@ -0,0 +1,24 @@
+/* ihmref-BTElement.java
+ * HISTORIQUE
+ *
+ * [MODIF]: Version - le 26 avr. 2015 - Creation de la classe.
+ *
+ * FIN HISTORIQUE
+ */
+package org.views.comp.panes.elements;
+
+import javax.swing.JLabel;
+
+import org.views.comp.panes.parents.ComposantPane;
+
+public class BTElement extends ComposantPane{
+
+	/** BTElement
+	 * Constructeur
+	 */
+	public BTElement(String libelle_) {
+		super(libelle_);
+	}
+
+	
+}
